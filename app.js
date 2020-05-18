266ab74a70d2053156aa699c71f5058edf1fe8da
sandesh181199 <sandeshvanwadi@gmail.com>
2020-05-18 14:42:38 +0530

    added address().port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000
+//const port = process.env.PORT || 3000
 
 const url = 'mongodb://sandeshvanwadi:sandesh246@ds039175.mlab.com:39175/heroku_kj33pdl1';
 const connect = mongoose.connect(url, {
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
 })
 
 const server = http.createServer(app);
-app.listen(port, () => {
-    console.log(`server running on ${port}`)
+app.listen(process.env.PORT || 3000 , () => {
+    // var port = server.address().port;
+    console.log(`server running on port`);
 })
